7a24e49b33a88cf2845c935d355115a2a62a53b3
meera-iyad <191128@ppu.edu.ps>
2023-10-12 12:05:30 +0300

    add, edit, delete, list, search for cities per country

--- script.mjs
+++ script.mjs
@@ -16,18 +16,21 @@ let findCountryIndex = (countryName) => {
   });
   return countryIndex;
 };
+
 let findCountryByName = (countryName) => {
   const filterCountries = countryList.find(
     (country) => country.name === countryName
   );
   return filterCountries;
 };
+
 let filterCountriesByName = (countrySubName) => {
   const filterCountries = countryList.filter((country) =>
     country.name.match(countrySubName)
   );
   return filterCountries;
 };
+
 let deleteCountryByName = (countryName) => {
   const index = findCountryIndex(countryName);
   countryList.splice(index, 1);
@@ -58,17 +61,55 @@ let combineTwoArrasOfCountries = (firstCountryArray, secondCountryArray) => {
   return firstCountryArray.concat(secondCountryArray);
 };
 
-console.log(reverseCountryList());
-console.log(sortCountryList());
-console.log(findCountryIndex("Yemen"));
-console.log(findCountryByName("Brazil"));
-console.log(filterCountriesByName("al"));
-console.log(getCountriesCount());
-deleteCountryByName("France");
-console.log(isCountryExist("Palestine"));
-console.log(removeLastCountry());
-console.log(removeFirstCountry());
-console.log(getCountriesCount());
-let firstSub = getSubArrayFromCountryListArray(54, 57);
-let secondSub = getSubArrayFromCountryListArray(58, 60);
-console.log(combineTwoArrasOfCountries(firstSub, secondSub));
+let addNewCountry = (newCountry) => {
+  countryList.push(newCountry);
+};
+
+let addNewCity = (country, newCity) => {
+  for (let i = 0; i < countryList.length; i++) {
+    if (countryList[i].name === country) {
+      countryList[i].cities.push(newCity);
+      return countryList[i];
+    }
+  }
+};
+
+let editCountry = (newCountry) => {
+  let countryIndex = findCountryIndex(newCountry.name);
+  if (countryIndex > -1) {
+    countryList.splice(countryIndex, 1, newCountry);
+  } else {
+    console.log("The country with the given id is not exist");
+  }
+};
+
+let listCountries = () => {
+  countryList.map((country) => {
+    console.log(country.name);
+  });
+};
+
+let searchByCity = (city) => {
+  for (let i = 0; i < countryList.length; i++) {
+    for (const element of countryList[i]["cities"]) {
+      if (element === city) return countryList[i];
+    }
+  }
+};
+
+let deleteCityInCountry = (city) => {
+  let country = searchByCity(city);
+  let cityIndex = country.cities.findIndex((element) => {
+    return element == city;
+  });
+  country.cities.splice(cityIndex, 1);
+  return country;
+};
+
+let listCities = (countryName) => {
+  let countryObject = findCountryByName(countryName);
+  for (let i = 0; i < countryObject["cities"].length; i++) {
+    console.log(countryObject["cities"][i]);
+  }
+};
+
